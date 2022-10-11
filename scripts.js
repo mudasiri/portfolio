d719d15b00130695ba39a76b43296b5f39a6679a
Mudasir Issah <danladi496@gmail.com>
2022-10-11 23:11:11 +0000

    fixed-js-errors

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ const menuIcon = document.querySelector('.fa-bars');
 const mobileMenuList = document.querySelector('.mobile-menu-pane');
 const mobileHeader = document.querySelector('.flexbox');
 const bodyS = document.querySelector('.body');
-const navItem = document.getElementsByClassName('nav-item');
 
 menuIcon.addEventListener('click', () => {
   mobileHeader.classList.toggle('header-bg');
@@ -11,18 +10,9 @@ menuIcon.addEventListener('click', () => {
   bodyS.classList.toggle('stopScroll');
 });
 
-/* navItem.addEventListener('click', () => {
-  mobileMenuList.classList.remove('opened');
-  menuIcon.classList.remove('fa-xmark');
-  mobileHeader.classList.remove('header-bg');
-  bodyS.classList.remove('stopScroll');
-  //bodyS.classList.remove('stopScroll');
-}); */
-
 function removeStuffs() {
   mobileMenuList.classList.remove('opened');
   menuIcon.classList.remove('fa-xmark');
   mobileHeader.classList.remove('header-bg');
   bodyS.classList.remove('stopScroll');
-  //bodyS.classList.remove('stopScroll');
 }
