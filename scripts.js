fecfcd5156db7700d9d727e667f939f440d0c76d
jossi <joepatriot30@gmail.com>
2022-10-11 13:27:36 +0300

    added a linking of the page with the menu

--- scripts.js
+++ scripts.js
@@ -7,3 +7,23 @@ menuIcon.addEventListener('click', () => {
   menuIcon.classList.toggle('fa-xmark');
   mobileMenuList.classList.toggle('opened');
 });
+const portfolio = document.querySelector('#portfolio');
+const about = document.querySelector('.#about');
+const contact = document.querySelector('.#contact');
+
+const close = () => {
+  mobileHeader.classList.toggle('header-bg');
+  menuIcon.classList.toggle('fa-xmark');
+  mobileMenuList.classList.toggle('opened');
+}
+portfolio.addEventListener('click', () => {
+  close();
+});
+
+about.addEventListener('click', () => {
+  close();
+});
+
+contact.addEventListener('click', () => {
+  close();
+});
