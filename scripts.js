90093ec10fdfd7069e6bdc8c2eab5a995c787b16
Mudasir Issah <danladi496@gmail.com>
2022-10-11 23:00:54 +0000

    added-remove-function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,28 @@
 const menuIcon = document.querySelector('.fa-bars');
 const mobileMenuList = document.querySelector('.mobile-menu-pane');
 const mobileHeader = document.querySelector('.flexbox');
+const bodyS = document.querySelector('.body');
+const navItem = document.getElementsByClassName('nav-item');
 
 menuIcon.addEventListener('click', () => {
   mobileHeader.classList.toggle('header-bg');
   menuIcon.classList.toggle('fa-xmark');
   mobileMenuList.classList.toggle('opened');
+  bodyS.classList.toggle('stopScroll');
 });
+
+/* navItem.addEventListener('click', () => {
+  mobileMenuList.classList.remove('opened');
+  menuIcon.classList.remove('fa-xmark');
+  mobileHeader.classList.remove('header-bg');
+  bodyS.classList.remove('stopScroll');
+  //bodyS.classList.remove('stopScroll');
+}); */
+
+function removeStuffs() {
+  mobileMenuList.classList.remove('opened');
+  menuIcon.classList.remove('fa-xmark');
+  mobileHeader.classList.remove('header-bg');
+  bodyS.classList.remove('stopScroll');
+  //bodyS.classList.remove('stopScroll');
+}
