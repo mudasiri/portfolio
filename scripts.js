09328299b179e2ae04495d6ab103729d705f9b59
Mudasir Issah <danladi496@gmail.com>
2022-10-12 00:09:41 +0000

    added-overlay

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const menuIcon = document.querySelector('.fa-bars');
 const mobileMenuList = document.querySelector('.mobile-menu-pane');
 const mobileHeader = document.querySelector('.flexbox');
 const bodyS = document.querySelector('.body');
+const demobtn = document.querySelector('.see-demo-btn');
 
 menuIcon.addEventListener('click', () => {
   mobileHeader.classList.toggle('header-bg');
@@ -16,3 +17,9 @@ function removeStuffs() {
   mobileHeader.classList.remove('header-bg');
   bodyS.classList.remove('stopScroll');
 }
+
+// create array to store projects
+// let projectInfo = {};
+demobtn.addEventListener('click', () => {
+  document.getElementById('overlay-project').style.display = 'block';
+});
